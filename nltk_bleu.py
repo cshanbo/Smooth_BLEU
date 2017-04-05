9199a33ddc6f64ff04a490c32dbba96d0f94b9be
Shanbo Cheng <cshanbO@gmail.com>
2017-04-05 13:18:37 +0800

    output file

--- nltk_bleu.py
+++ nltk_bleu.py
@@ -16,15 +16,26 @@ if __name__ == '__main__':
                          help="translation file")
     parser.add_argument('-sl', '--sentence-level', action='store_true',
                          help="print segment level BLEU score (default: %(default)s)")
+    parser.add_argument('-o', '--output', type=str, default='', required=False, 
+                        help="output BLEU score to this file in segment level 
+                              scenario (default: %(default)s)")
 
     args = parser.parse_args()
     if args.sentence_level:
         with open(args.translation, 'r') as trans, open(args.reference, 'r') as ref:
-            for tl in trans:
-                rl = ref.readline()
-                tran_list = tl.strip().split(' ')
-                ref_list = rl.strip().split(' ')
-                print(nltk.translate.bleu_score.sentence_bleu([ref_list], tran_list))
+            if args.output == '':
+                for tl in trans:
+                    rl = ref.readline()
+                    tran_list = tl.strip().split(' ')
+                    ref_list = rl.strip().split(' ')
+                    print(nltk.translate.bleu_score.sentence_bleu([ref_list], tran_list))
+            else:
+                with open(args.output, 'w') as outputfile:
+                    for tl in trans:
+                        rl = ref.readline()
+                        tran_list = tl.strip().split(' ')
+                        ref_list = rl.strip().split(' ')
+                        outputfile.write('%s\n' % nltk.translate.bleu_score.sentence_bleu([ref_list], tran_list))
     else:
         with open(args.translation, 'r') as trans, open(args.reference, 'r') as ref:
             tran_list, ref_list = [], []
@@ -33,4 +44,3 @@ if __name__ == '__main__':
                 tran_list.append(tl.strip().split(' '))
                 ref_list.append(rl.strip().split(' '))
         print(nltk.translate.bleu_score.corpus_bleu(ref_list, tran_list))
-
